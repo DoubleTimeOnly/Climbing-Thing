bfef753aa1a80bf2ea4e85cdcff659ef926133c3
Victor <doubletimeonly@gmail.com>
2022-08-06 16:17:10 -0400

    Commit right version this time

--- climbing_thing/route/groupholds.py
+++ climbing_thing/route/groupholds.py
@@ -29,19 +29,11 @@ def segment_routes(wall_image: np.ndarray, holds: Instances) -> Dict[str, List[i
     # iterative over all masks and get hold images
     batch: List[torch.Tensor] = []
     for idx, mask in enumerate(holds.masks):
-        mask = mask.to('cpu')   # same size as wall image
-        mask = np.array(mask.long()).astype(np.uint8)
         hold_bbox = holds.boxes[idx].tensor.int()   # 20x40 pixel magnitude
 
-        # masked_image = wall_image[mask > 0] # same size as wall_image
-        masked_image = cv2.bitwise_and(wall_image, wall_image, mask=mask)
-        # hold_image = wall_image[hold_bbox[0, 1]:hold_bbox[0, 3], hold_bbox[0, 0]:hold_bbox[0, 2]]
-        hold_image = masked_image[hold_bbox[0, 1]:hold_bbox[0, 3], hold_bbox[0, 0]:hold_bbox[0, 2]]
+        hold_image = wall_image[hold_bbox[0, 1]:hold_bbox[0, 3], hold_bbox[0, 0]:hold_bbox[0, 2]]
 
-        # debugging
-        # cv2.imshow("image", hold_image)
-        # cv2.waitKey()
-        hold_image = hold_image[..., ::-1].copy()
+        hold_image = hold_image[..., ::-1].copy()   # Convert BGR->RGB and torch can't handle negative indices :(
         batch.append(hold_image)
 
     prompts = COLORS
